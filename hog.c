b79fc4d3d2f0c1e229328c865ff5b80fb0330643
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-03-28 11:56:04 +0100

    Big clean

--- hog.c
+++ hog.c
@@ -4,11 +4,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 
 #include <errno.h>
 #include <malloc.h>
 #include <string.h>
+#include <strings.h>
 #include <time.h>
 
 #include <sched.h>
@@ -18,6 +20,9 @@
 /* ================================================================= */
 
 const long cOneMbyte = 1024 * 1024;
+
+const int TRUE = 1;
+const int FALSE = 0;
 
 /* ================================================================= */
 
@@ -38,12 +43,7 @@ void analyseCPUs()
 
 /* ================================================================= */
 
-static struct tms sDummyTms;
-static clock_t sStartTime;
-static int sTargetLoad;
-static time_t sUsleepTime = 0;
-static clock_t sLastCPUTimeUsed = 0;
-static double sWrappedCPUTime = 0.0;
+// #define DEBUG
 
 #ifdef DEBUG
 #define DEBUG_FREQUENCY 1000
@@ -52,17 +52,22 @@ static int sDebugNow = DEBUG_FREQUENCY;
 
 /* ================================================================= */
 
-void adjustLoad()
+void adjustLoad(clock_t startTime, int load)
 {
+  static time_t sUsleepTime = 0;
+  static clock_t sLastCPUTimeUsed = 0;
+  static double sWrappedCPUTime = 0.0;
+
+  struct tms _tms;
   clock_t currentTime;
   clock_t elapsedTime;
   clock_t CPUTimeUsed;
   double totalCPUTimeUsed;
   int currentLoad;
 
-  currentTime = times(&sDummyTms);
+  currentTime = times(&_tms);
 
-  elapsedTime = currentTime - sStartTime;
+  elapsedTime = currentTime - startTime;
   elapsedTime = elapsedTime * 10; /* Now in msecs */
 
   CPUTimeUsed = clock() / 1000;
@@ -78,13 +83,18 @@ void adjustLoad()
 
   currentLoad = (int)(100.5 * totalCPUTimeUsed / (double)elapsedTime);
 
-  if (currentLoad >= sTargetLoad)
+  if (currentLoad > load)
   {
-    sUsleepTime = sUsleepTime * 2 + sTargetLoad;
+    sUsleepTime = sUsleepTime * 2;
 
-    if (sUsleepTime > 999999)
+    if (sUsleepTime < 100)
     {
-      sUsleepTime = 999999;
+      sUsleepTime = 100;
+    }
+
+    if (sUsleepTime >= 1000000)
+    {
+      sUsleepTime = 1000000;
     }
   }
   else
@@ -94,6 +104,7 @@ void adjustLoad()
 
   if (sUsleepTime != 0)
   {
+    // printf("Sleeping %d us\n", sUsleepTime);
     (void)usleep(sUsleepTime);
   }
 
@@ -114,6 +125,9 @@ void adjustLoad()
 
 /* ================================================================= */
 
+// Track number of grabs
+static long sGrabCycle = 0;
+
 // Hold onto the previosly grabbed memory for a cycle
 static char *spPrevGrabbedMemory = 0;
 
@@ -127,11 +141,13 @@ static char *spGrabbedMemoryCurrent = 0;
 int grabMemory(amount)
 long amount;
 {
+  sGrabCycle++;
+
   int returnValue = 0;
 
   if (amount > 0)
   {
-    printf("\nGrabbing %d Mbytes of memory ...\n", amount / cOneMbyte);
+    printf("\nGrabbing %d Mbytes of memory [#%d]...\n", amount / cOneMbyte, sGrabCycle);
 
     if (spPrevGrabbedMemory != 0)
     {
@@ -160,9 +176,8 @@ long amount;
 
 /* ================================================================= */
 
-// Spinner chars to display
-static int sSpinPos = 0;
-static char *sSpinner = "o)";
+const char *SPINNER_BODY = ":-";
+const char *DEFAULT_SPINNER = "o)";
 
 // After how many full memory update cyles do we regrab
 const int REGRAB_START = 20;
@@ -170,42 +185,56 @@ static int regrab = REGRAB_START;
 
 // We don't need to set all bytes to make it resident
 const int MEM_STRIDE = 64;
+const int STRIDES_PER_CALL = 64;
 
 void useMemory(long amount, const char *spinner)
 {
+  static int spinPos = 0;
+
   if (spGrabbedMemory != 0)
   {
-    *spGrabbedMemoryCurrent = 0xAA;
-
-    spGrabbedMemoryCurrent += MEM_STRIDE;
-
-    if (spGrabbedMemoryCurrent > spGrabbedMemoryEnd)
+    if (spGrabbedMemoryCurrent == spGrabbedMemoryStart && strcmp(spinner, "NOSPINNER") != 0)
     {
-      if (strcmp(sSpinner, "NOSPINNER") != 0)
+      printf("\r%s%c", SPINNER_BODY, spinner[spinPos++]);
+      fflush(stdout);
+
+      if (spinner[spinPos] == '\0')
+        spinPos = 0;
+    }
+
+    for (int i = 0; i < STRIDES_PER_CALL; i++)
+    {
+      *spGrabbedMemoryCurrent = 0xAA;
+
+      spGrabbedMemoryCurrent += MEM_STRIDE;
+
+      if (spGrabbedMemoryCurrent > spGrabbedMemoryEnd)
       {
-        printf("\r%.*s%c", strlen(spinner) - 1, spinner, sSpinner[sSpinPos++]);
-        fflush(stdout);
+        regrab--;
+        if (regrab == 0)
+        {
+          grabMemory(amount);
+          regrab = REGRAB_START;
+        }
 
-        if (sSpinner[sSpinPos] == '\0')
-          sSpinPos = 0;
+        spGrabbedMemoryCurrent = spGrabbedMemoryStart;
+
+        // Start from beginning next time...
+        break;
       }
-
-      regrab--;
-      if (regrab == 0)
-      {
-        grabMemory(amount);
-        regrab = REGRAB_START;
-      }
-
-      spGrabbedMemoryCurrent = spGrabbedMemoryStart;
     }
   }
 }
 
 /* ================================================================= */
 
+const int NO_CPU = -1;
+
 void pinCpu(int cpu)
 {
+  if (cpu == NO_CPU)
+    return;
+
   cpu_set_t cpuset;
 
   CPU_ZERO(&cpuset);
@@ -222,7 +251,7 @@ void pinCpu(int cpu)
   }
   else
   {
-    printf("OK: Pinned to pin to CPU %d\n", cpu);
+    printf("OK: Pinned to CPU %d\n", cpu);
   }
 }
 
@@ -232,24 +261,20 @@ void showHelp()
 {
   static char *help =
       "hog\n"
-      "===\n"
       "\n"
       "Memory, CPU and CPU load hog utility.\n"
       "\n"
       "I want to hog ...\n"
       "\n"
       "A CPU - Effectively disabling one on a multi CPU machine\n"
-      "========================================================\n"
       "\n"
       "./hog 0 <CPU>  -- <CPU> is the number of the CPU to TOTALLY hog\n"
       "\n"
       "And some memory while I'm at it\n"
-      "===============================\n"
       "\n"
       "./hog <MEM> <CPU>  -- <MEM> is the number of megabytes to hog <CPU> as above\n"
       "\n"
       "Memory and no CPU\n"
-      "=================\n"
       "\n"
       "./hog <MEM> 0 0  -- <MEM> is the number of megabytes to hog\n"
       "\n"
@@ -259,7 +284,6 @@ void showHelp()
       "result in some memory being swapped out as a result\n"
       "\n"
       "Memory and some CPU\n"
-      "===================\n"
       "\n"
       "./hog <MEM> 0 <LOAD>  -- <MEM> as above and <LOAD> is the %% load on the CPU\n"
       "which in this case is 0 - use this for a single CPU\n"
@@ -268,13 +292,11 @@ void showHelp()
       "./hog <MEM> <CPU> <LOAD>  -- as above except choose <CPU> too\n"
       "\n"
       "Just some CPU\n"
-      "=============\n"
       "\n"
       "./hog 0 <CPU> <LOAD>  -- just set <MEM> to 0\n"
       "\n"
       "\n"
       "Resonable Values\n"
-      "================\n"
       "\n"
       "<MEM>	Up to the max allowed for your process and within sensible\n"
       "limits for the machine - asking for too much will lead to\n"
@@ -290,18 +312,27 @@ void showHelp()
 
 /* ================================================================= */
 
-int main(argc, argv, envp)
-int argc;
-char *argv[];
-char *envp[];
+const char *OPTIONS[] = {
+    "-help",
+    "help"};
+
+int isAnyOf(char *str, const char *options[])
 {
-  long memory = 8 * cOneMbyte;
-  int cpu = 0;
-  int returnValue = 0;
+  int res = FALSE;
 
-  sTargetLoad = 50;
+  for (int i = 0; i < sizeof(OPTIONS) / sizeof(char *); i++)
+  {
+    res = strcasecmp(str, options[i]) == 0;
+    if (res)
+      break;
+  }
 
-  if ((argc == 1) || ((argc >= 1) && ((argv[1][0] == '-') || (argv[1][0] == 'h') || (argv[1][0] == 'H'))))
+  return res;
+}
+
+void showHelpIfNeeded(int argc, char *argv[])
+{
+  if ((argc == 1) || ((argc >= 1) && (isAnyOf(argv[1], OPTIONS))))
   {
     showHelp();
 
@@ -309,68 +340,127 @@ char *envp[];
 
     exit(0);
   }
+}
+
+/* ================================================================= */
+
+const long ONE_MB = 1024 * 1024;
+
+long megabytesToBytes(int mb)
+{
+  return mb * ONE_MB;
+}
+
+double bytesToMegabytes(long bytes)
+{
+  return (bytes * 1.0) / ONE_MB;
+}
+
+/* ================================================================= */
+
+const long DEFAULT_MEMORY = 8 * ONE_MB;
+const int DEFAULT_CPU = NO_CPU;
+const int DEFAULT_LOAD = 50;
+const int FULL_LOAD = 100;
+
+typedef struct
+{
+  long memory;
+  int cpu;
+  int load;
+  const char *spinner;
+  clock_t startTime;
+} Settings;
+
+void parseArgs(int argc, char *argv[], Settings *settings)
+{
+  settings->memory = DEFAULT_MEMORY;
+  settings->cpu = DEFAULT_CPU;
+  settings->load = DEFAULT_LOAD;
+  settings->spinner = DEFAULT_SPINNER;
+
+  struct tms _tms;
+  settings->startTime = times(&_tms);
+
+  showHelpIfNeeded(argc, argv);
 
   if (argc >= 2)
   {
-    memory = atoi(argv[1]) * cOneMbyte;
+    settings->memory = megabytesToBytes(atoi(argv[1]));
   }
 
   if (argc >= 3)
   {
-    cpu = atoi(argv[2]);
-    sTargetLoad = 100;
+    settings->cpu = atoi(argv[2]);
+    settings->load = FULL_LOAD;
   }
 
   if (argc >= 4)
   {
-    sTargetLoad = atoi(argv[3]);
+    settings->load = atoi(argv[3]);
   }
 
   if (argc == 5)
   {
-    sSpinner = argv[4];
+    settings->spinner = argv[4];
   }
+}
 
-  printf("\nmemory = %d, cpu = %d, load = %d\n",
-         memory,
-         cpu,
-         sTargetLoad);
+/* ================================================================= */
 
-  returnValue = grabMemory(memory);
+void loadRunner(Settings *settings)
+{
+
+  int returnValue = grabMemory(settings->memory);
 
   if (returnValue != 0)
   {
     exit(returnValue);
   }
 
-  sStartTime = times(&sDummyTms);
-
   int i = 0;
-  int adjustFrequency = ((sTargetLoad / 5) + 1) * 4096;
+  int adjustFrequency = ((settings->load / 5) + 1) * 4096;
 
-  pinCpu(cpu);
+  pinCpu(settings->cpu);
 
-  const char *spinner = ":-)";
-
-  printf("\nPress Ctrl-C to exit ...\n%s", spinner);
+  printf("\nPress Ctrl-C to exit ...\n");
   fflush(stdout);
 
   for (;;)
   {
-    useMemory(memory, spinner);
+    useMemory(settings->memory, settings->spinner);
 
     i++;
 
-    if (sTargetLoad < 100)
+    if (settings->load < 100)
     {
       if ((i % adjustFrequency == 0))
       {
         i = 0;
 
-        adjustLoad();
+        adjustLoad(settings->startTime, settings->load);
       }
     }
   }
+}
+
+/* ================================================================= */
+
+int main(argc, argv)
+int argc;
+char *argv[];
+{
+  Settings settings;
+
+  parseArgs(argc, argv, &settings);
+
+  printf("\nmemory = %4.0lfMB, cpu = %d, load = %d, spinner = '%s'\n",
+         bytesToMegabytes(settings.memory),
+         settings.cpu,
+         settings.load,
+         settings.spinner);
+
+  loadRunner(&settings);
 }
 
 /* ================================================================= */
